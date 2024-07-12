e06160f07b792930a4f0945d4707508194eb5598
hebert benigno <hebertramos09@gmail.com>
2024-07-12 15:33:52 -0300

    Cleaning the project.

--- src/main/java/com/microserviceprojects/emailsender/consumers/EmailConsumer.java
+++ src/main/java/com/microserviceprojects/emailsender/consumers/EmailConsumer.java
@@ -1,8 +1,8 @@
 package com.microserviceprojects.emailsender.consumers;
 
+import com.microserviceprojects.emailsender.senders.EmailSender;
 import com.microserviceprojects.emailsender.dtos.EmailDto;
 import com.microserviceprojects.emailsender.entities.Email;
-import com.microserviceprojects.emailsender.servicies.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +13,14 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer {
 
     @Autowired
-    EmailService emailService;
+    EmailSender emailSender;
 
-    // =>  Method that will listen the queue
-    @RabbitListener(queues = "${spring.rabbitmq.queue}") // To show tho the Spring that method is a listener of a queue
+    @RabbitListener(queues = "${spring.rabbitmq.queue}") // Shows that it's a listener of a specific queue
     public void listen(@Payload EmailDto emailDto){
         Email emailModel = new Email();
         BeanUtils.copyProperties(emailDto, emailModel);
-        emailService.sendEmail(emailModel);
+        emailSender.sendEmail(emailModel);
+
         System.out.println("Email status: " + emailModel.getStatusEmail().toString());
     }
 }
